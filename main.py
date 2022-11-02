61e8f4fb89b759fa578c804d92c62fecc2abbf33
Jose Briones <josmbrio@gmail.com>
2022-11-02 14:57:15 -0500

    resync

---
+++ main.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv
+from flask import Flask
+from routes.api import info
+from routes.token import authorization
+from flask_jwt_extended import JWTManager
+from datetime import timedelta
+from redis_connection import jwt_redis_blocklist
+
+application = Flask(__name__)
+
+application.config["JWT_SECRET_KEY"] = "xxlpirqixmes"
+application.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=1)
+application.config["JWT_HEADER_NAME"] = 'X-JWT-KWY'
+application.config["JWT_HEADER_TYPE"] = ""
+
+jwt = JWTManager(application)
+
+
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload: dict):
+    jti = jwt_payload["jti"]
+    token_in_redis = jwt_redis_blocklist.get(jti)
+    return token_in_redis is not None
+
+
+application.register_blueprint(authorization)
+application.register_blueprint(info)
+
+
+if __name__ == '__main__':
+#    load_dotenv()
+    application.run(debug=True, port='5000', host='localhost')
