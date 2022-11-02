1a9b0d99cbece74431065797ae46e0d6421d0c78
Jose Briones <josmbrio@gmail.com>
2022-11-02 02:07:39 -0500

    Improvements to the code and usage of flask_jwt_extended

--- routes/api.py
+++ routes/api.py
@@ -1,34 +1,29 @@
+from datetime import timedelta
 from flask import Blueprint, request, jsonify
-from json_web_token import check_token
+from flask_jwt_extended import jwt_required, get_jwt
+from redis_connection import jwt_redis_blocklist
+
 
 info = Blueprint('info', __name__)
 
 
-@info.before_request
-def validate_jwt():
-    token = request.headers.get('X-JWT-KWY')
-    print(token)
-    return check_token(token)
-
-
 @info.route('/DevOps', methods=['POST'])
+@jwt_required()
 def get_info():
-    print(request.json)
     api_key = '2f5ae96c-b558-4c7b-a590-a501ae1c3f6c'
-    try:
-        request_api_key = request.headers.get('X-Parse-REST-API-Key')
-        print(request_api_key)
-        if request_api_key == api_key:
-            var_to = request.json['to']
-            return jsonify({"message": "Hello " + var_to + " your message will be send"})
-        else:
-            return jsonify({"message": "Incorrect API KEY"})
-    except KeyError:
-        return jsonify({"message": "You must defined the key X-Parse-REST-API-Key"})
+    request_api_key = request.headers.get('X-Parse-REST-API-Key')
+    if request_api_key == api_key:
+        var_to = request.json['to']
+        jti = get_jwt()["jti"]
+        jwt_redis_blocklist.set(jti, "", ex=timedelta(minutes=1))
+        return jsonify({"message": "Hello " + var_to + " your message will be send"})
+    else:
+        return jsonify({"message": "Incorrect API KEY"})
 
 
 @info.errorhandler(405)
 def error_405(error=None):
+    print("ERRRORRRRRr 405")
     response = jsonify({"message": "ERROR"})
     response.status_code = 405
     return response
--- routes/token.py
+++ routes/token.py
@@ -1,23 +1,10 @@
-from flask import Blueprint, request, jsonify
-from json_web_token import create_token, check_token
+from flask import Blueprint, jsonify
+from flask_jwt_extended import create_access_token
 
 authorization = Blueprint('route_token', __name__)
 
 
 @authorization.route('/auth', methods=['POST'])
-def auth():
-    username = request.json['username']
-    password = request.json['password']
-    if username == 'ex-user' and password == 'pa55w0rd':
-        token = create_token(values=request.json)
-        return token
-    else:
-        response = jsonify({"message": "Bad credentials"})
-        response.status_code = 404
-        return response
-
-
-@authorization.route('/verify')
-def verify_token():
-    token = request.json['Authorization']
-    return check_token(token)
+def auth_token():
+    token = create_access_token(identity="ex-user")
+    return jsonify(token=token)
