327340b282d768d68a5cd52f3aeecbafb2fd7afc
Jose Briones <josmbrio@gmail.com>
2022-11-04 11:56:07 -0500

    Troubleshooting

--- application/redis_connection/redis_connection.py
+++ application/redis_connection/redis_connection.py
@@ -1,6 +1,12 @@
 import redis
+from os import getenv
 
+redis_host = getenv('REDIS_HOST')
+if redis_host is None:
+    redis_host = 'redis'
+
+print("Redis host: " + redis_host)
 jwt_redis_denylist = redis.StrictRedis(
-    host="redis", port=6379, db=0, decode_responses=True
+    host=redis_host, port=6379, db=0, decode_responses=True
 )
 
