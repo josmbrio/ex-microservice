6adcca4318d3e502564c230c1dac72d1ab8d9a24
Jose Briones <josmbrio@gmail.com>
2022-11-06 12:50:22 -0500

    Reorganizing Jenkinsfile code

--- application/test/test_function.py
+++ application/test/test_function.py
@@ -2,4 +2,4 @@ from util_functions.api_key_validation import is_valid_api_key
 
 
 def test_is_valid_api_key():
-    assert is_valid_api_key('xxxxxxxx') == False
+    assert is_valid_api_key('1q1q2w2w3e3e4r4r') is False
